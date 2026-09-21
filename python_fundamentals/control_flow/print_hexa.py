63a967e0ba0444831c1288c566fcfe66a2aff751
Ray <14826@holbertonstudents.com>
2026-09-22 09:55:13 +1000

    tightend string processing last input control

--- python_fundamentals/control_flow/print_hexa.py
+++ python_fundamentals/control_flow/print_hexa.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 newline = "{0} = 0x{0:x}\n"
-curline = ""
-for i in range(0, 98 + 1):
+curline = "0 = 0x0\n"
+for i in range(0 + 1, 98 + 1):
+    if i == 98:
+        newline = "{0} = 0x{0:x}"
+
     curline = curline + newline.format(i)
 
 print(curline)
