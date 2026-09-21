2007049beed19dc18689230aa52597bc2c479d91
Ray <14826@holbertonstudents.com>
2026-09-21 21:45:18 +1000

    fixed contional logic take three

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -6,9 +6,9 @@ if number >= 0:
 if number < 0:
     digit = number % -10
 
-if digit > 5 and != 0:
+if digit > 5:
     print(f"Last digit of {number} is {digit} and is greater than 5")
 if digit == 0:
     print(f"Last digit of {number} is {digit} and is 0")
-if digit < 6:
+if digit < 6 and not digit == 0:
     print(f"Last digit of {number} is {digit} and is less than 6 and not 0")
