4ab425255d1413b343949a8f0938510f17bb7a97
Ray <14826@holbertonstudents.com>
2026-09-22 16:52:31 +1000

    Fixed output line

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def uppercase(str):
+    newstr = ''
+    for i in str:
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            newstr = newstr + chr(ord(i)-32)
+        else:
+            newstr = newstr + i
+
+    print(newstr)
