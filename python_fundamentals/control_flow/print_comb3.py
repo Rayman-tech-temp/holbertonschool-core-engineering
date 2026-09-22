4d76fe4472c5deb032dcd00d6311aa3ebfa3cd96
Ray <14826@holbertonstudents.com>
2026-09-22 13:48:34 +1000

    fixed conditional logic to specs

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -2,11 +2,14 @@
 oneDigit = "0{0}"
 twoDigit = "{0}"
 delim = ", "
+i = 0
 
-for i in range(0, 89 + 1):
+while i < 90:
 
-    if i == 0:
+    if i == 1:
         curline = oneDigit.format(i) + delim
+    elif i > 9 and int(i % 10) == 0:
+        i = i + int(i / 10)
     elif i < 10 or i % 10 != int(i / 10) % 10:
         if i < 10 and i > 0:
             curline = curline + oneDigit.format(i) + delim
@@ -14,5 +17,5 @@ for i in range(0, 89 + 1):
             curline = curline + twoDigit.format(i) + delim
         elif i == 89:
             curline = curline + twoDigit.format(i)
-
+    i += 1
 print(curline)
