1402a864ebb78065a59c3c49d24eb587ceaaba23
Ray <14826@holbertonstudents.com>
2026-09-22 10:23:00 +1000

    tighten the string processing

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 newchr = '{0}'
-alphabt = ''
+alphabt = newchr
 for char in range(ord('a'), ord('z') + 1):
 
     if char != ord('q') and char != ord('e'):
-        alphabt = alphabt + newchr.format(chr(char))
+        if char == ord('a'):
+            alphabt = alphabt.format(chr(char))
+
+        else:
+            alphabt = alphabt + newchr.format(chr(char))
 
 print(alphabt)
