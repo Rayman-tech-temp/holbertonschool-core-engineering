5eda26a0b5495ff4c97a71034732c56afa668ab1
Ray <14826@holbertonstudents.com>
2026-09-22 17:16:50 +1000

    corrected scope of the function

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 def print_last_digit(number):
-    newstr = '{0}'
+
     if number >= 0:
         digit = number % 10
-    else:
+    elif number <= 0:
         digit = (number * -1) % 10
+    else:
+        digit = None
 
-    print(newstr.format(digit))
+    print(digit)
