b644e227fb89b841d1d6dc33fbfab780898ef1a3
Ray <14826@holbertonstudents.com>
2026-09-23 21:20:07 +1000

    submission task 6

---
+++ python_fundamentals/functions_modules/calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from add_0 import add, sub, div, mul
+a = 10
+b = 5
+txt = "{0}\n"
+
+if __name__ == "__main__":
+    c = add(a, b)
+    print(txt.format(c))
+    c = sub(a, b)
+    print(txt.format(c))
+    c = mul(a, b)
+    print(txt.format(c))
+    c = div(a, b)
+    print(txt.foramt(c))
