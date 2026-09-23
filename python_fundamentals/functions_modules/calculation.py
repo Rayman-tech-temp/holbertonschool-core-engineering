f1486bf0fb29969d3ba562ed0b5799ff0fcf6910
Ray <14826@holbertonstudents.com>
2026-09-23 22:03:16 +1000

    fixed errors with extra '(' in format

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -8,12 +8,15 @@ if __name__ == "__main__":
     txt = "{0} + {1} = {2}\n"
     c = add(a, b)
     print(txt.format(a, b, c))
+
     txt = "{0} - {1} = {2}\n"
     c = sub(a, b)
-    print(txt.format((a, b, c))
-    txt = "{0} / {1} = {2}\n"
-    c = div(a, b)
-    print(txt.foramt((a, b, c))
+    print(txt.format(a, b, c))
+
     txt = "{0} * {1} = {2}\n"
     c = mul(a, b)
-    print(txt.format((a, b, c))
+    print(txt.format(a, b, c))
+
+    txt = "{0} / {1} = {2}\n"
+    c = div(a, b)
+    print(txt.foramt(a, b, c))
