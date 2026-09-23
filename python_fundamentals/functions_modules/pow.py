9d68523bb0fa68527fd84efeaf24b98207840686
Ray <14826@holbertonstudents.com>
2026-09-23 13:33:55 +1000

    added negative powers functionality

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -4,7 +4,11 @@ def pow(a, b):
     i = 0
     answer = 1
     if a != 0 and b != 0:
-        for i in range(0, b):
-            answer = answer * a
+        if b > 0:
+            for i in range(0, b):
+                answer = answer * a
+        elif b < 0:
+            for i in range(0, b, -1):
+                answer = answer / a
 
     return (answer)
