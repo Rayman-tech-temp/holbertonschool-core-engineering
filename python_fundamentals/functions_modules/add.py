a83f1a14b8a27540ba2123e6805ca38317bc3a69
Ray <14826@holbertonstudents.com>
2026-09-23 19:12:47 +1000

    submitting task 5

---
+++ python_fundamentals/functions_modules/add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import add_0
+a = 1
+b = 2
+c = add_0.add(a, b)
+
+if __name__ == "__main__":
+    txt = f"{a} + {b} = {c}"
+    print(txt)
---
+++ python_fundamentals/functions_modules/add_0.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+def add(a, b):
+    if __name__ == "__main__":
+        output = a + b
+        return (output)
