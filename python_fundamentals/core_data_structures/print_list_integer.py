a29bba975d01f29572961fdaa148c346f5e49fce
Ray <14826@holbertonstudents.com>
2026-09-26 09:21:56 +1000

    submitting task 0 in correct dir

---
+++ python_fundamentals/core_data_structures/print_list_integer.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env python3
+
+def print_list_integer(a_list):
+    txt = "{:d}"
+    if a_list:
+        for i in a_list:
+        print(txt.format(i))
+    else:
+        print(a_list)
